88af5cdeac6afba090ee41cb98e3f0c2aa26c777
Shichenzia <2233801313@qq.com>
2021-07-04 20:25:36 +0800

    feat:修改姓名库的引入方式

--- src/data.js
+++ src/data.js
@@ -1,11 +1,35 @@
-'use strict';
+"use strict";
 
-const { libs } = nodex;
-const { mysql } = libs;
+const path = require("path");
 
+var nameLibsStore = {};
 
-exports.init = async function (args) { 
-  console.log('data init.');
-  console.log('data init args:', args);
-  await mysql.init(args.mysql);
+exports.init = async function (args) {
+  console.log("data init.");
+  console.log("data init args:", args);
+
+  for (const index in args.file) {
+    const fileName = path.join(process.cwd(), `data/name-libs/${args.file[index]}`);
+    const nameLibObj = requireLogic(fileName, args.file[index]);
+    
+    nameLibsStore = {
+      ...nameLibsStore,
+      ...nameLibObj
+    }
+  }
+};
+
+function requireLogic(fileName, file) {
+  const nameSubjiectList = require(fileName);
+  const subject = file.split('.')[0];
+
+  let nameLibObj = {};
+  nameLibObj[subject] = nameSubjiectList;
+
+  return nameLibObj;
+}
+
+exports.getSubjectNameLibs = async function(subject){
+  let subjectNameLibs = nameLibsStore[subject];
+  return subjectNameLibs;
 };
