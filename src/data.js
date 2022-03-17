2bd1f96c9de2cae76e770a6cfee626869bfc50b0
Shichenzia <2233801313@qq.com>
2022-03-17 18:59:40 +0800

    fix：整理逻辑

--- src/data.js
+++ src/data.js
@@ -11,7 +11,7 @@ exports.init = async function (args) {
   for (const index in args.file) {
     const fileName = path.join(process.cwd(), `data/name-libs/${args.file[index]}`);
     const nameLibObj = requireLogic(fileName, args.file[index]);
-    
+
     nameLibsStore = {
       ...nameLibsStore,
       ...nameLibObj
@@ -19,6 +19,12 @@ exports.init = async function (args) {
   }
 };
 
+/**
+ * 读取json文件，并解析数据
+ * @param {String} fileName 请求路径
+ * @param {String} file 文件名
+ * @returns 该主题的姓名库对象
+ */
 function requireLogic(fileName, file) {
   const nameSubjiectList = require(fileName);
   const subject = file.split('.')[0];
@@ -29,6 +35,11 @@ function requireLogic(fileName, file) {
   return nameLibObj;
 }
 
+/**
+ * 获取主题的姓名库
+ * @param {String} subject 主题名
+ * @returns 该主题对应的姓名库
+ */
 exports.getSubjectNameLibs = async function(subject){
   let subjectNameLibs = nameLibsStore[subject];
   return subjectNameLibs;
--- src/logic.js
+++ src/logic.js
@@ -14,26 +14,10 @@ exports.helloWorld = async function () {
 };
 
 exports.nameGenerator = async function ({ subject }) {
-
   let subjectNameLibs = await data.getSubjectNameLibs(subject) || {};
   let { firstNameList = [], lastNameList = [] } = subjectNameLibs;
   let name = getRandomName(firstNameList, lastNameList) || null;
   return name;
-};
-
-exports.nickNameGenerator = async function ({ nameNum = 1, nameLength = 3 }) {
-  let nameArr = [];
-  for (let i = 0; i < nameNum; i++) {
-    let name = "";
-    for (let j = 0; j < nameLength; j++) {
-      let unicodeNum = "";
-      unicodeNum = randomAccess(0x4e00, 0x9fa5).toString(16);
-      name += decodeUnicode(unicodeNum);
-    }
-    nameArr.push(name);
-  }
-
-  return nameArr;
 };
 
 // 获取指定范围内的随机数 [min, max)
@@ -56,14 +40,3 @@ function getRandomName(firstNameList, lastNameList) {
   return name;
 }
 
-// 解码
-function decodeUnicode(str) {
-  //Unicode显示方式是\u4e00
-  str = "\\u" + str;
-  str = str.replace(/\\/g, "%");
-  //转换中文
-  str = unescape(str);
-  //将其他受影响的转换回原来
-  str = str.replace(/%/g, "\\");
-  return str;
-}
